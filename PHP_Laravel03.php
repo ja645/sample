59b3bf1ebece6c8854a626aec6e2551ccfe7cb6d
Okayama <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-02-02 20:26:58 +0000

    PHP/Laravel 03 プログラムの実行を条件分岐構文と繰り返し構文で制御できるようになろう

---
+++ PHP_Laravel03.php
@@ -0,0 +1,46 @@
+<?php
+
+//課題1.
+$name = "岡山";
+
+if ($name == "岡山") {
+  echo "私は" . $name . "です";
+  echo "\n";
+} else {
+  echo $name . "ではありません";
+  echo "\n";
+}
+
+//2.
+$total = 0;
+
+for ($i = 0; $i <= 100; $i++) {
+  $total += $i;
+}
+
+echo $total;
+echo "\n";
+
+//3.
+$fruits = ["orange", "apple", "grape", "peach", 
+"strawberry"];
+
+foreach ($fruits as $fruit){
+  echo $fruit;
+  echo "\n";
+}
+
+//4.
+/* for文の始めの値を定義する */         //'/'を'*/'に変更
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){     //'<'を'<='に変更
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;                            //';'を追記　　　
+    echo "\n";                          //改行
+  }
+}
